0b0f55cd0b2fee314332526c1d5a84e07574586b
David Zuniga <ninod4764@gmail.com>
2021-01-17 02:41:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,7 @@ function basicTeenager(age) {'You are a teenager! if the age is age is between 1
 
 }
 
-function teenager(age) {
+function teenager(age) {'You are a teenager!' if the age is between 13-19:
 
 }
 
